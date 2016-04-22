1cff7fff916e7a5beab1befdd208f1a4a29900ae
Thien Luu <tn.luu1@gmail.com>
2016-04-22 17:12:47 -0500

    pdf upload

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,20 @@
 var express = require('express'),
 	router = express.Router(),
-	nunjucks = require('nunjucks')
+	nunjucks = require('nunjucks'),
+	fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index.html', { title: 'Thien Luu' });
+});
+
+/* GET pdf */
+router.get('/asset', function(request, response){
+  var tempFile="//floating-basin-41540.herokuapp.com/doc/GenResume03.26.pdf";
+  fs.readFile(tempFile, function (err,data){
+     response.contentType("application/pdf");
+     response.send(data);
+  });
 });
 
 module.exports = router;
